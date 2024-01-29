d9831daa094f477f8ff94df839f4251fcd19ee0e
Liyakhatimtiyazali <130666487+Liyakhatimtiyazali@users.noreply.github.com>
2024-01-29 15:01:41 +0530

    Create constant.py

---
+++ constant.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 30 18:34:46 2022
+
+@author: lenovo legion 5
+"""
+class constant:
+    
+    NotOK="Not found"
+    
+    inpid="invalid product id"
+    
+    impname="invalid product name"
+    
+    cp="connection problem"
+    
+    dup="duplicate found"
+    
+    ok="ok"
+    
+    pcart=[]
+    
+    os="out of stock"
+    
+    p="paid"
